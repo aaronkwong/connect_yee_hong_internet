1c2d87ab9ba711a00c83f950ce607ea0a89f255b
Aaron Wong <wongakk7@gmail.com>
2023-02-20 00:16:40 -0500

    add lines to connect_yee_hong_universal.py which will read in vars from the identity file containing vars specific to the PC it is running on. Can easily switch out identity file to configure for PC it is running on.

--- universal_connection/connect_yee_hong_universal.py
+++ universal_connection/connect_yee_hong_universal.py
@@ -13,23 +13,37 @@ import os
 
 #vars
 #directory where logs will be saved
-working_directory="C://Users//Aaron Wong//Desktop//connect_yee_hong_internet//universal_connection"
-#name of the wifi adapter
-five_ghz_adapter_name="Wi-Fi 3"
-#the script to initate hotspot turn off
-path_to_turn_OFF_hotspot_script="C://Users//Aaron Wong//Desktop//connect_yee_hong_internet//universal_connection//active_scripts//turn_OFF_hotspot.ps1"
-#ther script to initiate hotspot tunr on 
-path_to_turn_ON_hotspot_script="C://Users//Aaron Wong//Desktop//connect_yee_hong_internet//universal_connection//active_scripts//turn_on_hotspot.ps1"
-#whether to run the hotspot
-run_hotspot=False
-#path to revboot script
-path_reboot_bat="C://Users//Aaron Wong//Desktop//connect_yee_hong_internet//universal_connection//active_scripts//reboot.bat"
-#name of profile of internet to connect to in case Yee Hong is unsuccessful. Must have connected previously to establish a saved profile
-backup_wifi="Dragon_backup"
+# working_directory="C://Users//Aaron Wong//Desktop//connect_yee_hong_internet//universal_connection"
+# #name of the wifi adapter
+# five_ghz_adapter_name="Wi-Fi 3"
+# #the script to initate hotspot turn off
+# path_to_turn_OFF_hotspot_script="C://Users//Aaron Wong//Desktop//connect_yee_hong_internet//universal_connection//active_scripts//turn_OFF_hotspot.ps1"
+# #ther script to initiate hotspot tunr on 
+# path_to_turn_ON_hotspot_script="C://Users//Aaron Wong//Desktop//connect_yee_hong_internet//universal_connection//active_scripts//turn_on_hotspot.ps1"
+# #whether to run the hotspot
+# run_hotspot=False
+# #path to revboot script
+# path_reboot_bat="C://Users//Aaron Wong//Desktop//connect_yee_hong_internet//universal_connection//active_scripts//reboot.bat"
+# #name of profile of internet to connect to in case Yee Hong is unsuccessful. Must have connected previously to establish a saved profile
+# backup_wifi="Dragon_backup"
 
+#import vars from identity
+from identity import working_directory
+from identity import five_ghz_adapter_name
+from identity import path_to_turn_OFF_hotspot_script
+from identity import path_to_turn_ON_hotspot_script
+from identity import run_hotspot
+from identity import path_reboot_bat
+from identity import backup_wifi
 
+print("working_directory wifi is: "+working_directory)
+print("five_ghz_adapter_name wifi is: "+five_ghz_adapter_name)
+print("path_to_turn_OFF_hotspot_script wifi is: "+path_to_turn_OFF_hotspot_script)
+print("path_to_turn_ON_hotspot_script wifi is: "+path_to_turn_ON_hotspot_script)
+print("run_hotspot wifi is: "+str(run_hotspot))
+print("path_reboot_bat wifi is: "+path_reboot_bat)
 print("backup wifi is: "+backup_wifi)
-print("The chrome driver path is: "+chrome_driver_path)
+
 
 if(os.path.isfile(path_to_turn_OFF_hotspot_script)):
     print("good")
--- universal_connection/identity/legion.py
+++ universal_connection/identity/legion.py
@@ -1,8 +1,6 @@
 #vars
 #directory where logs will be saved
 working_directory="C://Users//Aaron Wong//Desktop//connect_yee_hong_internet//universal_connection"
-#path to the chrome driver to be used
-chrome_driver_path="C://Users//Aaron Wong//Desktop//connect_yee_hong_internet//chromedriver_v109//chromedriver.exe"
 #name of the wifi adapter
 five_ghz_adapter_name="Wi-Fi"
 #the script to initate hotspot turn off
